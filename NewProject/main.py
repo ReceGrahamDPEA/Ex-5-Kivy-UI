96d1ff0e6f499a591beb8ff6e7327471479423cb
Rece Graham <recegraham@dpengineering.org>
2022-11-07 10:50:18 -0800

    Commiting a screen

--- NewProject/main.py
+++ NewProject/main.py
@@ -1,7 +1,12 @@
 import os
+import time
+import logging
+import threading
 
 os.environ['DISPLAY'] = ":0.0"
 os.environ['KIVY_WINDOW'] = 'egl_rpi'
+
+import os.path
 
 from kivy.app import App
 from kivy.animation import Animation
@@ -9,6 +14,7 @@ from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.slider import Slider
+from kivy.uix.button import Button
 
 from pidev.MixPanel import MixPanel
 from pidev.kivy.PassCodeScreen import PassCodeScreen
@@ -19,6 +25,7 @@ from pidev.kivy.selfupdatinglabel import SelfUpdatingLabel
 
 from kivy.properties import ObjectProperty
 
+import time as timetime
 from datetime import datetime
 
 time = datetime
@@ -30,7 +37,7 @@ SCREEN_MANAGER = ScreenManager()
 MAIN_SCREEN_NAME = 'main'
 ADMIN_SCREEN_NAME = 'admin'
 IMAGE_SCREEN_NAME = 'image'
-
+ANIMATED_SCREEN_NAME = 'animated'
 
 
 class ProjectNameGUI(App):
@@ -46,13 +53,55 @@ class ProjectNameGUI(App):
         """
         return SCREEN_MANAGER
 
+class DPEAButton(Button):
+    """
+    DPEAButton class
+    """
+    shadow_image_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images", "shadow.png")
+    shadow_path = ObjectProperty(shadow_image_path)
 
+    def __init__(self, **kwargs):
+        """
+        Specifies the background_color, background_normal, and size_hint for all instances
+        :param kwargs: Arguments passed to the Button Instance
+        """
+        super(DPEAButton, self).__init__(**kwargs)
+        self.background_color = (0, 0, 0, 0)
+        self.background_normal = ''
+        self.size_hint = (None, None)
+        self.original_colors = list()
+
+    def on_press(self):
+        """
+        Overrides the Button default on_press to darken the color of the button.
+        :return: None
+        """
+        super(DPEAButton, self).on_press()
+        self.original_colors = self.color
+        self.color = [i * 0.7 for i in self.original_colors]
+
+    def on_touch_up(self, touch):
+        """
+        Overrides the Button default on_touch_up to revert the buttons color back to its original color.
+        NOTE: This method is called for every widget onscreen
+        :return: None
+        """
+        super(DPEAButton, self).on_touch_up(touch)
+
+        # If button hasn't been pressed self.original colors is empty and will make the button color be black
+        # So if the length is empty it hasn't been pressed so we return
+        if len(self.original_colors) == 0:
+            return
+
+        self.color = self.original_colors
 
 
 Window.clearcolor = (1, 1, 1, 1)  # White
 
 
 class MainScreen(Screen):
+
+    Builder.load_file('AnimatedButtonScreen.kv')
 
     clicks = ObjectProperty()
 
@@ -81,9 +130,11 @@ class MainScreen(Screen):
 
     def basicAnimation(self, widget):
 
-        animation = Animation(pos=(100, 100), t = 'out_bounce')
-
+        animation = Animation(pos=(100, 100), t = 'out_bounce', duration = 1) + Animation(size=(200,200), duration=5)
         animation.start(widget)
+
+        SCREEN_MANAGER.current = 'animated'
+
         print("bless you")
 
 class ImageScreen(Screen):
@@ -98,6 +149,22 @@ class ImageScreen(Screen):
         """
         SCREEN_MANAGER.current = MAIN_SCREEN_NAME
 
+class AnimatedButtonScreen(Screen):
+
+    def complexAnimation(self, widget):
+        anim = Animation(pos=(80, 10))
+        anim &= Animation(size=(800, 800), duration=2.)
+        anim.start(widget)
+
+        SCREEN_MANAGER.current = 'main'
+
+    @staticmethod
+    def transition_back():
+        """
+        Transition back to the main screen
+        :return:
+        """
+        SCREEN_MANAGER.current = MAIN_SCREEN_NAME
 
 class AdminScreen(Screen):
     """
@@ -153,6 +220,7 @@ SCREEN_MANAGER.add_widget(PassCodeScreen(name='passCode'))
 SCREEN_MANAGER.add_widget(PauseScreen(name='pauseScene'))
 SCREEN_MANAGER.add_widget(AdminScreen(name=ADMIN_SCREEN_NAME))
 SCREEN_MANAGER.add_widget(ImageScreen(name='image'))
+SCREEN_MANAGER.add_widget(AnimatedButtonScreen(name='animated'))
 
 """
 MixPanel
